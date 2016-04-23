b56b27885ea19a542b7a1038aabde3be4d2ecbc3
tinyauyu <yu.tin.yau@gmail.com>
2016-04-23 15:49:20 +0800

    displacement detection

--- displacement/displacement.py
+++ displacement/displacement.py
@@ -2,6 +2,8 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
+init_pitch, init_roll, init_yaw = sense.get_orientation().values()
+
 while True:
     pitch, roll, yaw = sense.get_orientation().values()
-    print("pitch=%s, roll=%s, yaw=%s" % (pitch,yaw,roll))
+    print("pitch=%s, roll=%s, yaw=%s" % (pitch-init_pitch,yaw-init_yaw,roll-init_roll))
