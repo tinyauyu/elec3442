daa1d5d6dbd708b1df83868386d974f9e2d84867
tinyauyu <yu.tin.yau@gmail.com>
2016-04-25 16:15:17 +0800

    echo

---
+++ server-client/client.py
@@ -0,0 +1,15 @@
+import socket
+
+PORT = 12345
+
+# create an INET, STREAMing socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# now connect to the web server on port 80 - the normal http port
+s.connect(('192.168.1.103', PORT))
+
+s.send('hihi'.encode())
+reply = s.recv(1024)
+
+while True:
+	cmd = raw_input('Enter your command: ')
+	s.send(cmd.encode())
