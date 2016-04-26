6f15d0738252350351ee52085d7d4e544e795a30
tinyauyu <yu.tin.yau@gmail.com>
2016-04-26 18:35:02 +0800

    serial

--- serial.py
+++ serial.py
@@ -1,70 +1,8 @@
-"""
-Listen to serial, return most recent numeric values
-Lots of help from here:
-http://stackoverflow.com/questions/1093598/pyserial-how-to-read-last-line-sent-from-serial-device
-"""
-from threading import Thread
-import time
 import serial
 
-last_received = ''
-def receiving(ser):
-    global last_received
-    buffer = ''
-    while True:
-        buffer = buffer + ser.read(ser.inWaiting())
-        if '\n' in buffer:
-            lines = buffer.split('\n') # Guaranteed to have at least 2 entries
-            last_received = lines[-2]
-            #If the Arduino sends lots of empty lines, you'll lose the
-            #last filled line, so you could make the above statement conditional
-            #like so: if lines[-2]: last_received = lines[-2]
-            buffer = lines[-1]
-
-
-class SerialData(object):
-    def __init__(self, init=50):
-        try:
-            self.ser = ser = serial.Serial(
-                port='/dev/ttyUSB0',
-                baudrate=9600,
-                bytesize=serial.EIGHTBITS,
-                parity=serial.PARITY_NONE,
-                stopbits=serial.STOPBITS_ONE,
-                timeout=0.1,
-                xonxoff=0,
-                rtscts=0,
-                interCharTimeout=None
-            )
-        except serial.serialutil.SerialException:
-            #no serial connection
-            self.ser = None
-        else:
-            Thread(target=receiving, args=(self.ser,)).start()
-        
-    def next(self):
-        if not self.ser:
-            return 100 #return anything so we can test when Arduino isn't connected
-        #return a float value or try a few times until we get one
-        for i in range(40):
-            raw_line = last_received
-            try:
-                return float(raw_line.strip())
-            except ValueError:
-                print 'bogus data',raw_line
-                time.sleep(.005)
-        return 0.
-    def __del__(self):
-        if self.ser:
-            self.ser.close()
-
-if __name__=='__main__':
-    f = open('idle.txt','w')
-    s = SerialData()
-    for i in range(600):
-        time.sleep(.1)
-        in_num = s.next()
-        print in_num
-        f.write(str(in_num)) # python will convert \n to os.linesep
-        f.write('\n')
-    f.close() # you can omit in most cases as the destructor will call it
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.1, xonxoff=False, rtscts=False, dsrdtr=False) #Tried with and without the last 3 parameters, and also at 1Mbps, same happens.
+ser.flushInput()
+ser.flushOutput()
+While True:
+  data_raw = ser.readline()
+  print(data_raw)
