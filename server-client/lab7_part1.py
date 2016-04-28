c2d8801732e8f2ba2d5b8b3e5b28562fd205978e
tinyauyu <yu.tin.yau@gmail.com>
2016-04-29 01:31:11 +0800

    serial

--- server-client/lab7_part1.py
+++ server-client/lab7_part1.py
@@ -99,57 +99,57 @@ server.start()
 def SerialGetThread():
     ###### Serial #####
 
-last_received = ''
-def receiving(ser):
-    global last_received
-    buffer = ''
-    while True:
-        buffer = buffer + ser.read(ser.inWaiting())
-        if '\n' in buffer:
-            lines = buffer.split('\n') # Guaranteed to have at least 2 entries
-            last_received = lines[-2]
-            #If the Arduino sends lots of empty lines, you'll lose the
-            #last filled line, so you could make the above statement conditional
-            #like so: if lines[-2]: last_received = lines[-2]
-            buffer = lines[-1]
+    last_received = ''
+    def receiving(ser):
+        global last_received
+        buffer = ''
+        while True:
+            buffer = buffer + ser.read(ser.inWaiting())
+            if '\n' in buffer:
+                lines = buffer.split('\n') # Guaranteed to have at least 2 entries
+                last_received = lines[-2]
+                #If the Arduino sends lots of empty lines, you'll lose the
+                #last filled line, so you could make the above statement conditional
+                #like so: if lines[-2]: last_received = lines[-2]
+                buffer = lines[-1]
 
 
-class SerialData(object):
-    def __init__(self, init=50):
-        try:
-            self.ser = ser = serial.Serial(
-                port='/dev/ttyUSB0',
-                baudrate=9600,
-                bytesize=serial.EIGHTBITS,
-                parity=serial.PARITY_NONE,
-                stopbits=serial.STOPBITS_ONE,
-                timeout=0.1,
-                xonxoff=0,
-                rtscts=0,
-                interCharTimeout=None
-            )
-        except serial.serialutil.SerialException:
-            #no serial connection
-            self.ser = None
-        else:
-            Thread(target=receiving, args=(self.ser,)).start()
-        
-    def next(self):
-        if not self.ser:
-            return 100 #return anything so we can test when Arduino isn't connected
-        #return a float value or try a few times until we get one
-        for i in range(40):
-            raw_line = last_received
-            return raw_line
+    class SerialData(object):
+        def __init__(self, init=50):
             try:
-                return float(raw_line.strip())
-            except ValueError:
-                print 'bogus data',raw_line
-                time.sleep(.005)
-        return 0.
-    def __del__(self):
-        if self.ser:
-            self.ser.close()
+                self.ser = ser = serial.Serial(
+                    port='/dev/ttyUSB0',
+                    baudrate=9600,
+                    bytesize=serial.EIGHTBITS,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    timeout=0.1,
+                    xonxoff=0,
+                    rtscts=0,
+                    interCharTimeout=None
+                )
+            except serial.serialutil.SerialException:
+                #no serial connection
+                self.ser = None
+            else:
+                Thread(target=receiving, args=(self.ser,)).start()
+            
+        def next(self):
+            if not self.ser:
+                return 100 #return anything so we can test when Arduino isn't connected
+            #return a float value or try a few times until we get one
+            for i in range(40):
+                raw_line = last_received
+                return raw_line
+                try:
+                    return float(raw_line.strip())
+                except ValueError:
+                    print 'bogus data',raw_line
+                    time.sleep(.005)
+            return 0.
+        def __del__(self):
+            if self.ser:
+                self.ser.close()
 
     s = SerialData()
     while(True):
