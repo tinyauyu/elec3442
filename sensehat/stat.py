8dc68ea094688bc30c909975fa3a7580cb2b8130
tinyauyu <yu.tin.yau@gmail.com>
2016-04-29 04:41:39 +0800

    stat

--- sensehat/stat.py
+++ sensehat/stat.py
@@ -1,3 +1,6 @@
+import socket
+PORT = 12349
+
 from sense_hat import SenseHat
 sense = SenseHat()
 sense.clear()
@@ -9,3 +12,24 @@ while True:
 	pressure = sense.get_pressure()
 	pressure = round(pressure, 2)
 	print("temperature: %s, humidity: %s, pressure: %s" % temp, humidity, pressure)
+
+
+
+
+
+# create an INET, STREAMing socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# now connect to the web server on port 80 - the normal http port
+s.connect(('192.168.1.104', PORT))
+
+s.send('hihi'.encode())
+reply = s.recv(1024)
+
+while True:
+	t = "TEMP " + str(temp)
+	s.send(t.encode())
+	h = "HUMD " + str(humidity)
+	s.send(h.encode())
+	p = "PRES " + str(pressure)
+	s.send(p.encode())
+	time.sleep(5) 
