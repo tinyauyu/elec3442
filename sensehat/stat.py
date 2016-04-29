abcc6ac2b1bcfd22e205757d9989e47963b5dc67
Jim Chan <jim@jim.com>
2016-04-29 13:18:19 +0800

    client

--- sensehat/stat.py
+++ sensehat/stat.py
@@ -1,4 +1,4 @@
-PORT = 12300
+PORT = 12307
 
 import socket
 import time
@@ -24,7 +24,7 @@ s.send('hihi'.encode())
 reply = s.recv(1024)
 
 lastUpdate = time.time()
-
+init_pitch, init_roll, init_yaw = sense.get_orientation().values()
 f_x, f_y, f_z = sense.get_accelerometer_raw().values()
 
 f_x=round(f_x, 3)
@@ -52,15 +52,15 @@ dz = 0
 while True:
 	currTime = time.time()
 	if(currTime-lastUpdate>5):
-		temp = sense.get_temperature()
-		temp = round(temp,2)
-		humidity = sense.get_humidity()
-		humidity = round(humidity, 2)
-		pressure = sense.get_pressure()
-		pressure = round(pressure, 2)
-		pitch, roll, yaw = sense.get_orientation().values()
+	    temp = sense.get_temperature()
+	    temp = round(temp,2)
+	    humidity = sense.get_humidity()
+	    humidity = round(humidity, 2)
+	    pressure = sense.get_pressure()
+	    pressure = round(pressure, 2)
+	    pitch, roll, yaw = sense.get_orientation().values()
 
-		x, y, z = sense.get_accelerometer_raw().values()
+	    x, y, z = sense.get_accelerometer_raw().values()
 
 	    x=round(x, 3)
 	    y=round(y, 3)
@@ -103,8 +103,8 @@ while True:
 	    # print("dx=%s, dy=%s, dz=%s" % (dx, dy, dz))
 
 	    t = "ENVR " + str(temp) + " " + str(humidity) + " " + str(pressure) + " " + str(yaw - init_yaw) + " " + str(dx) + " " + str(dy) + " " + str(dz)
-		s.send(t.encode())
+            s.send(t.encode())
 
-		lastUpdate=currTime
+	    lastUpdate=currTime
 
 
